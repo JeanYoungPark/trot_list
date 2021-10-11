6d2d2917fae5dee2a3daea7c073b80cec57fdf6f
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-10-11 19:08:03 +0900

    네비게이션 레이아웃 설정

--- app/src/main/java/com/jean/trot_list/MainActivity.kt
+++ app/src/main/java/com/jean/trot_list/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //1. 네비게이션 만들기
+        //2. 프래그먼트 만들고 네비게이션에 연결
+            //2-1. 프래그먼트에서 버튼 누르면 이동
+        //3. 리사이클러 뷰 만들기
     }
 }
